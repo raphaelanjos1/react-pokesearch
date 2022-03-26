4cbb685d3f74f116e92992c9b458edab14220df9
raphaelanjos1 <raphael-anjos@live.com>
2022-03-26 16:09:52 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey, Welcome to PokeSearch App</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from './Theme'
+import { QueryClientProvider, QueryClient } from 'react-query'
+
+// Create a client
+const queryClient = new QueryClient()
+
+ReactDOM.render(
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>,
+  </QueryClientProvider>,
+
+  document.getElementById('root')
+)
